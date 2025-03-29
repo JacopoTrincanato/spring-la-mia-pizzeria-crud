e2dd9bdd0d786a36df0f67b7611ee3ea326c1ab7
JacopoTrincanato <trincanatoj@gmail.com>
2025-03-29 12:07:27 +0100

    Create OffertaSpecialeRepository.java

---
+++ java_pizzeria/src/main/java/org/lessons/java/pizzeria/java_pizzeria/repository/OffertaSpecialeRepository.java
@@ -0,0 +1,8 @@
+package org.lessons.java.pizzeria.java_pizzeria.repository;
+
+import org.lessons.java.pizzeria.java_pizzeria.models.OffertaSpeciale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OffertaSpecialeRepository extends JpaRepository<OffertaSpeciale, Integer> {
+
+}
